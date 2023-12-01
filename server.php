0a98fe25472cf86c6e9afa8a64a972850f7ce5ed
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-12-01 19:03:41 +0100

    BONUS 2 completed

--- server.php
+++ server.php
@@ -4,12 +4,19 @@ $filecontent = file_get_contents("todo-list.json");
 
 $list = json_decode($filecontent, true);
 
+//aggiunge un elemento alla lista
 if (isset($_POST['task'])) {
     $newtask = [
         "name" => $_POST['task'],
         "done" => false
     ];
     array_push($list, $newtask);
+    file_put_contents('todo-list.json', json_encode($list));
+}
+
+//cambia lo stato di un elemento della lista
+if (isset($_POST['index'])) {
+    $list[$_POST['index']]['done'] = !$list[$_POST['index']]['done'];
     file_put_contents('todo-list.json', json_encode($list));
 }
 
